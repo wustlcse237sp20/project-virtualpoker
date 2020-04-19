8151ac89701be0bce03d17d14ffb0db593d733cf
awasae <54609411+awasae@users.noreply.github.com>
2020-04-19 13:54:31 -0500

    testing

--- src/poker/CardSetTest.java
+++ src/poker/CardSetTest.java
@@ -51,5 +51,21 @@ class CardSetTest {
 		
 		assertEquals(clubs.get(0), card2);
 	}
+	
+	@Test
+	void shuffleRemove() {
+		ArrayList<Card> cards = new ArrayList<Card>();
+		Card card1 = new Card(Suit.CLUBS, Rank.ACE);
+		Card card2 = new Card(Suit.DIAMONDS, Rank.EIGHT);
+		Card card3 = new Card(Suit.DIAMONDS, Rank.ACE);
+		cards.add(card1);
+		cards.add(card2);
+		cards.add(card3);
+		
+		CardSet cardSet = new CardSet(cards);
+		ArrayList<Card> clubs = cardSet.removeByRank(Rank.ACE, 2);
+		
+		assertEquals(clubs.get(0), card2);
+	}
 
 }
